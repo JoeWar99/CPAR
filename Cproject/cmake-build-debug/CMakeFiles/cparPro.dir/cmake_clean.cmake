018478b9c889f1545c8f017a8f364c53778dd06b
LockdownPT <guerra70@live.com.pt>
2021-02-28 15:28:13 +0000

    Added java project and c project

---
+++ Cproject/cmake-build-debug/CMakeFiles/cparPro.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cparPro.dir/main.cpp.o"
+  "cparPro"
+  "cparPro.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cparPro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
