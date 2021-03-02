adeb9f90ef834dd5d74b4f614565ba5b022ba98f
LockdownPT <guerra70@live.com.pt>
2021-03-02 00:19:47 +0000

    Refactor java

---
+++ JavaProject/src/Main2.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+import java.lang.management.*;
+
+public class Main2 {
+    public double[] columnMultiplication(int size) {
+        double[] mat_a = new double[size * size];
+        double[] mat_b = new double[size * size];
+        double[] mat_c = new double[size * size];
+
+        ThreadMXBean th = ManagementFactory.getThreadMXBean();
+
+        for(int i = 0; i < size * size; ++i) {
+            mat_a[i] = 1;
+            mat_b[i] = i / size + 1;
+            mat_c[i] = 0;
+        }
+
+        long id = Thread.currentThread().getId();
+        long start = th.getThreadCpuTime(id);
+
+        for(int i = 0; i < size; ++i)
+            for(int j = 0; j < size; ++j) {
+                double temp = 0.0;
+                for(int k = 0; k < size; ++k)
+                    temp += mat_a[i * size + k] * mat_b[k * size + j];
+                mat_c[i * size + j] = temp;
+            }
+
+        long end = th.getThreadCpuTime(id);
+
+        for(int i = 0; i < Math.min(10, size); ++i)
+            System.out.print(mat_c[i] + " ");
+        System.out.println();
+        System.out.println("Time: " + (end-start)/1e9);
+
+        return mat_c;
+    }
+
+    public double[] lineMultiplication(int size) {
+        double[] mat_a = new double[size * size];
+        double[] mat_b = new double[size * size];
+        double[] mat_c = new double[size * size];
+
+        ThreadMXBean th = ManagementFactory.getThreadMXBean();
+
+        for(int i = 0; i < size * size; ++i) {
+            mat_a[i] = 1;
+            mat_b[i] = i / size + 1;
+            mat_c[i] = 0;
+        }
+
+        long id = Thread.currentThread().getId();
+        long start = th.getThreadCpuTime(id);
+        long start2 = System.nanoTime();
+
+        for(int i = 0; i < size; ++i)
+            for(int k = 0; k < size; ++k) {
+                double mat_a_ik = mat_a[i * size + k];
+                for(int j = 0; j < size; ++j)
+                    mat_c[i * size + j] += mat_a_ik * mat_b[k * size + j];
+            }
+
+        long end = th.getThreadCpuTime(id);
+        long end2 = System.nanoTime();
+
+        for(int i = 0; i < Math.min(10, size); ++i)
+            System.out.print(mat_c[i] + " ");
+        System.out.println();
+        System.out.println("Time1: " + (end-start)/1e9);
+        System.out.println("Time2: " + (end2-start2)/1e9);
+
+        return mat_c;
+    }
+
+    public static void main(String[] args) {
+        Main2 m = new Main2();
+        System.out.println("1. Multiplication");
+        System.out.println("2. Line Multiplication");
+
+        Scanner reader = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("Selection: ");
+            int option = reader.nextInt();
+
+            if(option == 0) break;
+
+            System.out.print("Size: ");
+            int size = reader.nextInt();
+
+            switch(option) {
+                case 1:
+                    m.columnMultiplication(size);
+                    break;
+                case 2:
+                    m.lineMultiplication(size);
+                default:
+                    break;
+            }
+        }
+    }
+}
--- JavaProject/src/MyMainClass.java
+++ JavaProject/src/MyMainClass.java
@@ -1,3 +1,5 @@
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -5,23 +7,14 @@ import java.util.Scanner;
 public class MyMainClass {
 
     public void printResultAndTime(long start, int size, double[] phc){
+        System.out.println("Time passed in seconds: " + (System.currentTimeMillis()-start)/1000F);
+
         System.out.println("Result matrix: ");
         for(int i=0; i<1; i++)
-        {	for(int j=0; j<Math.min(10,size); j++)
+        {	for(int j=0; j<Math.min(10,size * size); j++)
             System.out.print(phc[j] + " ");
         }
         System.out.println("");
-
-        // Get elapsed time in milliseconds
-        long elapsedTimeMillis = System.currentTimeMillis()-start;
-        // Get elapsed time in seconds
-        float elapsedTimeSec = elapsedTimeMillis/1000F;
-        // Get elapsed time in minutes
-        float elapsedTimeMin = elapsedTimeMillis/(60*1000F);
-
-        System.out.println("Time passed in milliseconds: " + elapsedTimeMillis);
-        System.out.println("Time passed in seconds: " + elapsedTimeSec);
-        System.out.println("Time passed in minutes: " + elapsedTimeMin);
     }
 
 
@@ -29,28 +22,27 @@ public class MyMainClass {
         double[] pha = new double[size * size];
         double[] phb = new double[size * size];
         double[] phc = new double[size * size];
-        int temp;
-        
-        Arrays.fill(pha, 1.0);
-        Arrays.fill(phc, 0.0);
+        int i, j, k;
+        double temp;
 
-        // Get current time
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
+                pha[i * size + j] = (double)1.0;
+                phb[i * size + j] = (double)(i + 1);
+            }
+        }
+
         long start = System.currentTimeMillis();
 
-        for(int i=0; i<size; i++)
-            for(int j=0; j<size; j++)
-                phb[i*size + j] = i+1;
-
-        for(int i=0; i<size; i++)
-        {	for(int j=0; j<size; j++) {
+        for(i=0; i<size; i++)
+        {	for(j=0; j<size; j++) {
                 temp = 0;
-                for(int k=0; k<size; k++) {
+                for(k=0; k<size; k++) {
                     temp += pha[i*size+k] * phb[k*size+j];
                 }
                 phc[i*size+j]=temp;
             }
         }
-
         printResultAndTime(start, size, phc);
     }
 
@@ -58,26 +50,25 @@ public class MyMainClass {
         double[] pha = new double[size * size];
         double[] phb = new double[size * size];
         double[] phc = new double[size * size];
-        int temp;
-        
-        Arrays.fill(pha, 1.0);
-        Arrays.fill(phc, 0.0);
+        int i, k, j;
 
-        // Get current time
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
+                pha[i*size + j] = (double)1.0;
+                phb[i*size + j] = (double)(i+1);
+                phc[i*size + j] = 0;
+            }
+        }
+
         long start = System.currentTimeMillis();
 
-        for(int i=0; i<size; i++)
-            for(int j=0; j<size; j++)
-                phb[i*size + j] = i+1;
-
-        for(int i=0; i<size; i++)
-        {	for(int k=0; k<size; k++) {
-                for (int j = 0; j < size; j++) {
+        for(i=0; i<size; i++)
+        {	for(k=0; k<size; k++) {
+                for (j = 0; j < size; j++) {
                     phc[i * size + j] += pha[i * size + k] * phb[k * size + j];
                 }
             }
         }
-
         printResultAndTime(start, size, phc);
     }
 
