ddee5afba213300b39c37edb3e74ddcac21845fc
Roberto Mourato <up201705616@fe.up.pt>
2021-05-23 17:53:41 +0100

    aiai open mp a bombar

--- Project2/blockMultiplication.cpp
+++ Project2/blockMultiplication.cpp
@@ -4,7 +4,7 @@
 #include <iomanip>
 #include <ctime>
 #include <cstdlib>
-#include <papi.h>
+#include <chrono>
 
 #ifdef _OPENMP
   #define TRUE  1
@@ -17,14 +17,12 @@
 #endif
 
 using namespace std;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::seconds;
 
-#define SYSTEMTIME clock_t
-
-void printResultAndTime(SYSTEMTIME Time1, int size, double *phc, char *st){
-    SYSTEMTIME Time2 = clock();
-    sprintf(st, "Time: %3.3f seconds\n", (double)(Time2 - Time1) / CLOCKS_PER_SEC);
-    cout << st;
-
+void printResultAndTime(int size, double *phc){
     cout << "Result matrix: " << endl;
     for(int i=0; i<1; i++)
     {	for(int j=0; j<min(10,size * size); j++)
@@ -33,12 +31,8 @@ void printResultAndTime(SYSTEMTIME Time1, int size, double *phc, char *st){
     cout << endl;
 }
 
-void OnMultBlock(int size, int blockSize)
+void OnMultBlockSequential(int size, int blockSize)
 {
-    SYSTEMTIME Time1;
-
-    char st[100];
-    double temp;
     int i0, i, j0, j, k0, k;
 
     double* pha, * phb, * phc;
@@ -55,7 +49,7 @@ void OnMultBlock(int size, int blockSize)
         }
     }
 
-    Time1 = clock();
+    auto t1 = high_resolution_clock::now();
 
     for (i0 = 0; i0 < size; i0 += blockSize) {
         for (k0 = 0; k0 < size; k0 += blockSize) {
@@ -71,19 +65,19 @@ void OnMultBlock(int size, int blockSize)
         }
     }
 
-    printResultAndTime(Time1, size, phc, st);
+    auto t2 = high_resolution_clock::now();
+    auto s_int = duration_cast<seconds>(t2 - t1);
+    std::cout << s_int.count() << "s\n";
+
+    printResultAndTime(size, phc);
 
     free(pha);
     free(phb);
     free(phc);
 }
 
-void OnMultBlockOpenMP1(int size, int blockSize)
+void OnMultBlockOpenMP(int size, int blockSize)
 {
-    SYSTEMTIME Time1;
-
-    char st[100];
-    double temp;
     int i0, i, j0, j, k0, k;
 
     double* pha, * phb, * phc;
@@ -100,8 +94,8 @@ void OnMultBlockOpenMP1(int size, int blockSize)
         }
     }
 
-    int chunk = 100;
-    Time1 = clock();
+    int chunk = 1;
+    auto t1 = high_resolution_clock::now();
 
     #pragma omp parallel shared(pha, phb, phc, size, chunk) private(i, j, k, i0, j0, k0)
         {
@@ -121,79 +115,16 @@ void OnMultBlockOpenMP1(int size, int blockSize)
             }
         }
 
-    printResultAndTime(Time1, size, phc, st);
+    auto t2 = high_resolution_clock::now();
+    auto s_int = duration_cast<seconds>(t2 - t1);
+    std::cout << s_int.count() << "s\n";
+
+    printResultAndTime(size, phc);
 
     free(pha);
     free(phb);
     free(phc);
 }
-
-void OnMultBlockOpenMP2(int size, int blockSize)
-{
-    SYSTEMTIME Time1;
-
-    char st[100];
-    double temp;
-    int i0, i, j0, j, k0, k;
-
-    double* pha, * phb, * phc;
-
-    pha = (double*)malloc((size * size) * sizeof(double));
-    phb = (double*)malloc((size * size) * sizeof(double));
-    phc = (double*)malloc((size * size) * sizeof(double));
-
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
-            pha[i * size + j] = (double)1.0;
-            phb[i * size + j] = (double)(i + 1);
-            phc[i * size + j] = 0;
-        }
-    }
-
-    Time1 = clock();
-
-    for (i0 = 0; i0 < size; i0 += blockSize) {
-        for (k0 = 0; k0 < size; k0 += blockSize) {
-            for (j0 = 0; j0 < size; j0 += blockSize) {
-                #pragma omp parallel for collapse(2)
-                for (i = i0; i < min(i0 + blockSize, size); i++) {
-                    for (k = k0; k < min(k0 + blockSize, size); k++) {
-                        for (j = j0; j < min(j0 + blockSize, size); j++) {
-                            #pragma omp critical
-                            phc[i * size + j] += pha[i * size + k] * phb[k * size + j];
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    printResultAndTime(Time1, size, phc, st);
-
-    free(pha);
-    free(phb);
-    free(phc);
-}
-
-void handle_error (int retval)
-{
-    printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
-    exit(1);
-}
-
-void init_papi() {
-    int retval = PAPI_library_init(PAPI_VER_CURRENT);
-    if (retval != PAPI_VER_CURRENT && retval < 0) {
-        printf("PAPI library version mismatch!\n");
-        exit(1);
-    }
-    if (retval < 0) handle_error(retval);
-
-    std::cout << "PAPI Version Number: MAJOR: " << PAPI_VERSION_MAJOR(retval)
-              << " MINOR: " << PAPI_VERSION_MINOR(retval)
-              << " REVISION: " << PAPI_VERSION_REVISION(retval) << "\n";
-}
-
 
 int main (int argc, char *argv[])
 {
@@ -213,33 +144,14 @@ int main (int argc, char *argv[])
     int size, nt=1;
     int op, blockSize, numProcessors;
 
-    int EventSet = PAPI_NULL;
     long long values[2];
     int ret;
-
-
-    ret = PAPI_library_init( PAPI_VER_CURRENT );
-    if ( ret != PAPI_VER_CURRENT )
-        std::cout << "FAIL" << endl;
-
-
-    ret = PAPI_create_eventset(&EventSet);
-    if (ret != PAPI_OK) cout << "ERRO: create eventset" << endl;
-
-
-    ret = PAPI_add_event(EventSet,PAPI_L1_DCM );
-    if (ret != PAPI_OK) cout << "ERRO: PAPI_L1_DCM" << endl;
-
-
-    ret = PAPI_add_event(EventSet,PAPI_L2_DCM);
-    if (ret != PAPI_OK) cout << "ERRO: PAPI_L2_DCM" << endl;
 
     op=1;
     do {
         cout << endl << "1. Block Multiplication Sequential" << endl;
-        cout << "2. Block Multiplication Parallel OpenMP1" << endl;
-        cout << "3. Block Multiplication Parallel OpenMP2" << endl;
-        cout << "4. Block Multiplication SYCL" << endl;
+        cout << "2. Block Multiplication Parallel OpenMP" << endl;
+        cout << "3. Block Multiplication SYCL" << endl;
         cout << "Selection?: ";
         cin >>op;
         if (op == 0)
@@ -253,45 +165,17 @@ int main (int argc, char *argv[])
         numProcessors = min(numProcessors, omp_get_num_procs());
         omp_set_num_threads(numProcessors);
 
-        // Start counting
-        ret = PAPI_start(EventSet);
-        if (ret != PAPI_OK) cout << "ERRO: Start PAPI" << endl;
-
         switch (op){
             case 1:
-                OnMultBlock(size, blockSize);
+                OnMultBlockSequential(size, blockSize);
+                OnMultBlockOpenMP(size, blockSize);
                 break;
             case 2:
-                OnMultBlockOpenMP1(size, blockSize);
-                break;
-            case 3:
-                OnMultBlockOpenMP2(size, blockSize);
+                OnMultBlockOpenMP(size, blockSize);
                 break;
             case 4:
-                OnMultBlock(size, blockSize);
+                OnMultBlockSequential(size, blockSize);
                 break;
         }
-
-        ret = PAPI_stop(EventSet, values);
-        if (ret != PAPI_OK) cout << "ERRO: Stop PAPI" << endl;
-        printf("L1 DCM: %lld \n",values[0]);
-        printf("L2 DCM: %lld \n",values[1]);
-
-        ret = PAPI_reset( EventSet );
-        if ( ret != PAPI_OK )
-            std::cout << "FAIL reset" << endl;
-
     }while (op != 0);
-
-    ret = PAPI_remove_event( EventSet, PAPI_L1_DCM );
-    if ( ret != PAPI_OK )
-        std::cout << "FAIL remove event" << endl;
-
-    ret = PAPI_remove_event( EventSet, PAPI_L2_DCM );
-    if ( ret != PAPI_OK )
-        std::cout << "FAIL remove event" << endl;
-
-    ret = PAPI_destroy_eventset( &EventSet );
-    if ( ret != PAPI_OK )
-        std::cout << "FAIL destroy" << endl;
 }
--- Project2/joao.cpp
+++ Project2/joao.cpp
@@ -11,6 +11,12 @@
 #include <iomanip>
 #include <time.h>
 #include <cstdlib>
+#include <chrono>
+
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::seconds;
 
 void print_matrix(double *M, int side)
 {
@@ -48,6 +54,7 @@ void omp_multiplication(int side, int block_size)
 
     std::cout << "START|";
     time1 = clock();
+    auto t1 = high_resolution_clock::now();
 
     #pragma omp parallel for \
         default(shared) \
@@ -69,11 +76,79 @@ void omp_multiplication(int side, int block_size)
         }
     }
 
+    auto t2 = high_resolution_clock::now();
     time2 = clock();
+
+    auto s_int = duration_cast<seconds>(t2 - t1);
+    std::cout << s_int.count() << "s\n";
+
     stream << std::fixed << std::setprecision(2) << (double)(time2 - time1) / CLOCKS_PER_SEC;
     std::cout << "END in " << stream.str() << " seconds" << std::endl;
 
-    std::cout << "Result matrix:" << std::endl;
+    std::cout << "Result matrix parallel:" << std::endl;
+    print_matrix(C, side);
+
+    free(A);
+    free(B);
+    free(C);
+}
+
+void omp_multiplication_sequential(int side, int block_size)
+{
+    double *A, *B, *C;
+    clock_t time1, time2;
+    std::stringstream stream;
+    int i, j, k;
+    int first_index, index, last_index;
+    int block, last_block = side * side / block_size;
+
+    A = (double *)malloc((side * side) * sizeof(double));
+    B = (double *)malloc((side * side) * sizeof(double));
+    C = (double *)malloc((side * side) * sizeof(double));
+
+    for (i = 0; i < side; i++)
+    {
+        for (j = 0; j < side; j++)
+        {
+            A[i * side + j] = (double)1;
+            B[i * side + j] = (double)(i + 1);
+        }
+    }
+
+    std::cout << "START|";
+    time1 = clock();
+    auto t1 = high_resolution_clock::now();
+/*
+    #pragma omp parallel for \
+        default(shared) \
+        private(block, first_index, index, last_index)*/
+    for (block = 0; block < last_block; block++)
+    {
+        first_index = block * block_size;
+        last_index = std::min(side * side, first_index + block_size);
+/*
+        #pragma omp parallel for \
+            default(shared) \
+            private(index, i, j, k)*/
+        for (index = first_index; index < last_index; index++)
+        {
+            i = index / side;
+            j = index % side;
+            for (k = 0; k < side; k++)
+                C[i * side + k] += A[i * side + j] * B[j * side + k];
+        }
+    }
+
+    auto t2 = high_resolution_clock::now();
+    time2 = clock();
+
+    auto s_int = duration_cast<seconds>(t2 - t1);
+    std::cout << s_int.count() << "s\n";
+
+    stream << std::fixed << std::setprecision(2) << (double)(time2 - time1) / CLOCKS_PER_SEC;
+    std::cout << "END in " << stream.str() << " seconds" << std::endl;
+
+    std::cout << "Result matrix sequential:" << std::endl;
     print_matrix(C, side);
 
     free(A);
@@ -101,6 +176,7 @@ int main(int argc, char *argv[])
         exit(1);
 
     omp_multiplication(matrix_size, block_size);
+    omp_multiplication_sequential(matrix_size, block_size);
 
     return 0;
 }
